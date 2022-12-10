6e563b38728f170419518aa36117ebdd9b44f744
dayaniravi123 <ravidayani46@gmail.com>
2022-12-09 20:19:00 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Editor from "@monaco-editor/react";
+import * as Y from 'yjs';
+import { WebsocketProvider } from 'y-websocket';
+import { MonacoBinding } from 'y-monaco';
+import * as monaco from 'monaco-editor';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const ydoc = new Y.Doc()
+  const provider = new WebsocketProvider('wss://demos.yjs.dev', 'monaco-demo', ydoc)
+  const type = ydoc.getText('monaco')
+  
+  const editor = monaco.editor.create((document.getElementById('myeditor')), {
+    defaultValue: "// Welcome to My Editor",
+    height: '90vh',
+    language: 'javascript',
+    theme: 'vs-dark',
+    minimap: {
+      enabled: false
+    }
+  })
+  const monacoBinding = new MonacoBinding(type, (editor.getModel()), new Set([editor]), provider.awareness)
+
+ 
 }
 
 export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCzU2jzaI1jfhGjceaiT6gnQCliZrkbDPI",
+    authDomain: "myeditor-bafec.firebaseapp.com",
+    databaseURL: "https://myeditor-bafec-default-rtdb.firebaseio.com",
+    projectId: "myeditor-bafec",
+    storageBucket: "myeditor-bafec.appspot.com",
+    messagingSenderId: "795548210555",
+    appId: "1:795548210555:web:7217f7abfb0f898c8d99de",
+    measurementId: "G-CFKXN95ZYJ"
+};
+
+export default firebaseConfig;
